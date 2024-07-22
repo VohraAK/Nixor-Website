462104ab1e442560a4122c9c74e18db6d8ec7905
VohraAK <abdullah.k.vohra@gmail.com>
2024-07-22 17:10:16 +0500

    created student model

---
+++ backend/models/studentModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const subjectGradeSchema = new mongoose.Schema({
+    subject: {type: String, required: true},
+    // percentages
+    midterm: {type: Number, default: null},
+    final: {type: Number, default: null},
+    grade: {type: String, default: null}
+});
+
+// new students linked from user doc
+const studentSchema = new mongoose.Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    subjectGrades: {type: subjectGradeSchema, required: true}, // each subject has a grade object, with midterm and final marks
+});
