802814aea71a6002422371069208c2337873cc0b
VohraAK <abdullah.k.vohra@gmail.com>
2024-08-08 23:08:48 +0500

    added timestamps to student model

--- backend/models/studentModel.js
+++ backend/models/studentModel.js
@@ -4,16 +4,19 @@ const subjectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   midterm: { type: Number, default: null }, // percentage
   final: { type: Number, default: null }, // percentage
-  grade: { type: String, enum: ['A', 'B', 'C', 'D', 'E', 'F'], default: null }, // letter grade (A-F)
+  grade: { type: String, enum: ["A", "B", "C", "D", "E", "F"], default: null }, // letter grade (A-F)
 });
 
 // new students linked from user doc
-const studentSchema = new mongoose.Schema({
-  fullName: { type: String, required: true, default: null },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  subjectGrades: { type: [subjectSchema], default: null }, // each subject has a grade object, with midterm and final marks
-  ECAs: {type: Array, default: []}, // Footballer, Basketballer, Volleyballer, President, Secretary, Treasurer, etc 
-});
+const studentSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true, default: null },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    subjectGrades: { type: [subjectSchema], default: null }, // each subject has a grade object, with midterm and final marks
+    ECAs: { type: Array, default: [] }, // Footballer, Basketballer, Volleyballer, President, Secretary, Treasurer, etc
+  },
+  { timestamps: true }
+);
 
 const Student = mongoose.model("Student", studentSchema);
 
